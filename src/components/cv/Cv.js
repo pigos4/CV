0d01a21f20f4bba6a5a332e95533e0fa5691bd41
Daniel <64493498+pigos4@users.noreply.github.com>
2021-02-27 12:54:38 +0000

    cv

---
+++ src/components/cv/Cv.js
@@ -0,0 +1,73 @@
+function Cv (){
+
+
+
+    return(<div><p>
+        <h1>Daniel Piga Curriculum Vitae</h1>
+        <h3>
+        Daniel Piga<br></br>
+        49 Market Square<br></br>
+        London<br></br>
+        E14 6BU<br></br>
+        Tel: 07523231049<br></br>
+        Email: pigos487@gmail.com 
+        </h3>
+        <h2>Personal Statement</h2><br></br>
+        <p>
+        I am a forward-thinking individual who is driven to continuously self-improve and I thrive on the challenge. I have demonstrated my ability to self-learn and adapt through the co-founding of my fresh produce business Piga Ortofrutta.
+        
+        I am highly motivated and strive to improve myself and those around me. I have seen how teamwork can drive both efficiency and a different perspective. This is clearly highlighted in my current role at Brandy Melville, where effective teamwork is required to meet the high demands especially through the increase caused by COVID-19.
+        </p>
+        <h2>Work History:  </h2>
+        <h3>SSC Service for Brandy Melville – Picker/ Packer (May 2020 – Present)</h3>
+        I work within a very fast paced environment where the demand has increased due to COVID-19. Here I have seen how effective teamwork can yield strong results.
+        
+        My previous experience in team leadership has been of great benefit to ensuring the team is able to adapt quickly accurately process domestic and international packages while maintaining high quantity control in a rapidly changing environment.
+        
+        <h3>Imperial Treasure – Commis Waiter (October 2019 – May 2020)</h3>
+        This position was very similar to my previous and I took a lot of what I learnt from D & D London and apply this to my role in Imperial Treasure. I found how my role could impact the customers experience through the preparation and presentation of the food theatre areas which in turn created a polished ambiance.
+        
+        I thoroughly enjoyed seeing the affect my work had on the customers and which then generated repeat clientele. I was given additional responsibilities such as servicing customer which I found to be a rewarding experience, ensuring all the guests I served would leave impressed.
+        
+        <h3>D & D London – Commis Waiter (July 2019 – October 2019)</h3>
+        My role at D & D London gave me my first experience with dealing with customers in a restaurant environment.  I worked within a face-paced team assisting and support my colleagues were needed to ensure guests needs were meet in a timely manner.
+        
+        Additionally, sometimes I had to handle client concerns such as the timeliness of their order and meeting the high standards of the restaurant.
+        
+        
+        <h3>Hakkasan group - Kitchen Porter (January 2019 – June 2019)</h3>
+        In my first role within the UK, I was hired as a kitchen porter. My main responsibilities were ensuring the cleanliness of the preparation environment and maintaining the stock levels of equipment were maintained.
+        
+        I personally ensured high standards of cleanliness within the environment and would support others to adhere to the same standards and expectations.
+        
+        <h3>Mattia Puddu (Italy) - Sales Assistant (January 2018 – January 2019)</h3>
+        To gain further experience in computer construction I volunteered my assistance in exchange for learning and tuition. 
+        
+        I took the concepts and learnings from my producer wholesaler business and applied them to Mattia Puddu in order to improve customer engagement and ensure returning clientele.
+        
+        <h3>Piga Ortofrutta - Wholesale Fruit and Vegetable (October 2006 – January 2019)</h3>
+        Having gained extensive experience working with my father in the Italian street markets, I then went on to co-founded our family business at the age of 18. Myself and my brother were keen to build upon my father’s legacy.
+        
+        We started by purchasing 1 wholesaler and 3 small shops, with the mindset that the customers and community should be at the heart of what we do.
+        
+        Our community-driven approach enabled us to expand at a pace. As with all business, there was a range of unexpected challenges which focus on our resolve and belief in the community and customer foundations of the company. By 2007 we took the company from small business to a €2M business, employing 20 employees.
+        
+        <h2>Additional Skills</h2>
+        
+        Driver license: A, B, C and CPC
+        Languages: English, Italian
+        
+        <h2>Hobbies:</h2>
+        My passion for understand how things work has led me to programming. This gives me a mental stimulus, as I enjoy learning how to build custom websites and applications for the challenge of the ever-changing landscape of the technical world we live in. 
+        
+        My love for travel is linked to my interest in photograph and the opportunity and challenge of capturing new landscapes and vistas.
+        
+        I enjoy motorsports which I have followed for over a decade and participate in a range of motor activity’s when the time permits.
+        
+        
+        <h2>Referees: Reference available on request</h2></p></div>)
+
+}
+
+
+export default Cv;
